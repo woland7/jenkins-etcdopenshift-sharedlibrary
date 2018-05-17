cd7c30770d7bd60637b3e7a6b3dac61bf0d27289
woland7 <antonio.deiasio@gmail.com>
2018-05-17 17:32:20 +0200

    Print message that binary has already been downloaded.

--- vars/log.groovy
+++ vars/log.groovy
@@ -6,5 +6,8 @@ def info(message) {
         sh "curl -L https://github.com/woland7/etcdwatcher-openshift/releases/download/first/etcdwatcher-openshift -o etcdwatcher"
         sh "chmod +x ./etcdwatcher"
     }
+    else {
+        echo "Binary has already been download on first build."
+    }
     sh "./etcdwatcher"
 }
