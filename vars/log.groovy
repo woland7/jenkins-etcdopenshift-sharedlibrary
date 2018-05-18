762b2eef4a25f3711be6c1837d113f497c94d50c
woland7 <antonio.deiasio@gmail.com>
2018-05-18 15:43:52 +0200

    Testing etcdhelper with keys finally.

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,13 +1,5 @@
 def info(message) {
-    echo "INFO: ${message}"
-    def filePath =  "./etcdwatcher"
-    def file = new File(filePath)
-    if(!file.exists()){
-        sh "curl -L https://github.com/woland7/etcdwatcher-openshift/releases/download/first/etcdwatcher-openshift -o etcdwatcher"
-        sh "chmod +x ./etcdwatcher"
-    }
-    else {
-        echo "Binary has already been download on first build."
-    }
-    sh "./etcdwatcher"
+    sh "curl -L https://github.com/woland7/etcdwatcher-openshift/releases/download/first/etcdwatcher-openshift -o etcdwatcher"
+    sh "chmod +x ./etcdwatcher"
+    sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key dump"
 }
