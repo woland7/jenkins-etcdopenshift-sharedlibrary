79dfc239aadbdab1cc0b031f2294c795ad47a29b
woland7 <antonio.deiasio@gmail.com>
2018-05-18 17:23:09 +0200

    Try get command

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,5 +1,5 @@
 def info(message) {
-    sh "curl -L https://github.com/woland7/etcdwatcher-openshift/releases/download/v1.1/etcdwatcher-openshift -o etcdwatcher"
+    sh "curl 192.168.10.31:80/etcdhelper -o etcdwatcher"
     sh "chmod +x ./etcdwatcher"
     sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key get /kubernetes.io/pods/ciao1/badpod"
 }
