7c629d22e41e43dd246c1173468a0bc689c19116
woland7 <antonio.deiasio@gmail.com>
2018-05-21 14:52:38 +0200

    Parameterized namespace.

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -1,6 +1,6 @@
-def watchPodReadyState(pod) {
-    echo "Watch ready state of $pod"
+def watchPodReadyState(namespace, pod) {
+    sh "Watch ready state of $pod"
     sh "curl 192.168.10.31:80/etcdhelper -o etcdwatcher"
     sh "chmod +x ./etcdwatcher"
-    sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watch /kubernetes.io/pods/ciao1/$pod"
+    sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watch /kubernetes.io/pods/$namespace/$pod"
 }
