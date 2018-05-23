263119a10649c2d810d9468c6b62bf7b272f471e
woland7 <antonio.deiasio@gmail.com>
2018-05-23 12:54:33 +0200

    A try

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -1,7 +1,7 @@
-def watchPodReadyState(namespace, pod) {
+def watchPodReadyState(pod) {
     echo "Hello Antonio"
     echo "Watch ready state of $pod"
     sh "curl 192.168.10.31:80/etcdhelper -o etcdwatcher"
     sh "chmod +x ./etcdwatcher"
-    sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watch /kubernetes.io/pods/$namespace/$pod"
+    sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watch /kubernetes.io/pods/ciao1/$pod"
 }
