a0900add47d7b90ab5524fc77bfb871c22ee5caa
woland7 <antonio.deiasio@gmail.com>
2018-05-28 16:18:30 +0200

    Added timeout parameter to function.

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -1,7 +1,5 @@
-def watchPodReadyState(namespace, pod) {
-    echo "Hello Antonio"
-    echo "Watch ready state of $pod"
+def watchPodReadyState(namespace, pod, timeout) {
     sh "curl 192.168.10.31:80/etcdhelper -o etcdwatcher"
     sh "chmod +x ./etcdwatcher"
-    sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watch /kubernetes.io/pods/$namespace/$pod"
+    sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watch /kubernetes.io/pods/$namespace/$pod $timeout"
 }
