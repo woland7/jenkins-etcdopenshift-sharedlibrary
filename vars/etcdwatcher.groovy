f2a9bd04db86c77927992ef1cf10fb3add246d22
woland7 <antonio.deiasio@gmail.com>
2018-05-29 14:59:56 +0200

    Add try-catch block.

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -1,5 +1,10 @@
 def watchPodReadyState(namespace, pod, timeout) {
-    sh "curl 192.168.10.31:80/etcdhelper -o etcdwatcher"
-    sh "chmod +x ./etcdwatcher"
-    sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watch /kubernetes.io/pods/$namespace/$pod $timeout"
+    try {
+        sh "curl 192.168.10.31:80/etcdhelper -o etcdwatcher"
+        sh "chmod +x ./etcdwatcher"
+        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watch /kubernetes.io/pods/$namespace/$pod $timeout"
+    }
+    catch (exc){
+        echo "Operation could not be completed on time."
+    }
 }
