df2a8d4a59096f2755b068cfe4d7e4332c98f743
woland7 <antonio.deiasio@gmail.com>
2018-06-01 16:07:53 +0200

    \n wasn't handled. For the moment I just removed it.

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -12,8 +12,7 @@ def watchBuildWithTimeout(namespace, key, timeout, deployment) {
     try {
         sh "curl 192.168.10.24:80/etcdhelper -o etcdwatcher"
         sh "chmod +x ./etcdwatcher"
-        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchBuildWithTimeout /kubernetes.io/pods/$namespace/$key $timeout " +
-                "$deployment"
+        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchBuildWithTimeout /kubernetes.io/pods/$namespace/$key $timeout $deployment"
     }
     catch (exc){
         echo "Operation could not be completed on time. Do something."
