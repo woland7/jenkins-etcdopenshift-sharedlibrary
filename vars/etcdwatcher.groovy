f456bd94cf4e8c6551621bdc8e49e2dfb4a7d179
woland7 <antonio.deiasio@gmail.com>
2018-06-01 15:27:33 +0200

    Watch for build.
    Added watch for build.

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -1,8 +1,19 @@
-def watchPodReadyState(namespace, pod, timeout) {
+def watchWithTimeout(namespace, key, timeout) {
     try {
         sh "curl 192.168.10.24:80/watcher -o etcdwatcher"
         sh "chmod +x ./etcdwatcher"
-        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watch /kubernetes.io/pods/$namespace/$pod $timeout"
+        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchWithTimeout /kubernetes.io/pods/$namespace/$key $timeout"
+    }
+    catch (exc){
+        echo "Operation could not be completed on time. Do something."
+    }
+}
+def watchBuildWithTimeout(namespace, key, timeout, name, deployment) {
+    try {
+        sh "curl 192.168.10.31:80/etcdhelper -o etcdwatcher"
+        sh "chmod +x ./etcdwatcher"
+        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchBuildWithTimeout /kubernetes.io/pods/$namespace/$key $timeout $name " +
+                "$deployment"
     }
     catch (exc){
         echo "Operation could not be completed on time. Do something."
