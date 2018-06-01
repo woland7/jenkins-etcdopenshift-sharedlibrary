497b3130e45afd51aceba52867772033b651a995
woland7 <antonio.deiasio@gmail.com>
2018-06-01 03:06:34 +0200

    Update etcdwatcher.groovy

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -1,6 +1,6 @@
 def watchPodReadyState(namespace, pod, timeout) {
     try {
-        sh "curl 192.168.10.31:80/etcdhelper -o etcdwatcher"
+        sh "curl 192.168.10.24:80/etcdhelper -o etcdwatcher"
         sh "chmod +x ./etcdwatcher"
         sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watch /kubernetes.io/pods/$namespace/$pod $timeout"
     }
