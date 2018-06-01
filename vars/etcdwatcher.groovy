1a2c89e936093ef7170c758182068f09b529a1d3
woland7 <antonio.deiasio@gmail.com>
2018-06-01 16:13:28 +0200

    Hot fix.

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -10,7 +10,7 @@ def watchWithTimeout(namespace, key, timeout) {
 }
 def watchBuildWithTimeout(namespace, key, timeout, deployment) {
     try {
-        sh "curl 192.168.10.24:80/etcdhelper -o etcdwatcher"
+        sh "curl 192.168.10.24:80/watcher -o etcdwatcher"
         sh "chmod +x ./etcdwatcher"
         sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchBuildWithTimeout /kubernetes.io/pods/$namespace/$key $timeout $deployment"
     }
