30e353587b4afdcb4b75a43c35ae3821182cfddd
woland7 <antonio.deiasio@gmail.com>
2018-06-02 14:13:51 +0200

    Minor edit.
    Delete redundant parameter.
    
    Changed CLI command.

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -2,17 +2,17 @@ def watchWithTimeout(namespace, key, timeout) {
     try {
         sh "curl 192.168.10.24:80/watcher -o etcdwatcher"
         sh "chmod +x ./etcdwatcher"
-        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchWithTimeout /kubernetes.io/pods/$namespace/$key $timeout"
+        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchWithTimeout $namespace $key $timeout"
     }
     catch (exc){
         echo "Operation could not be completed on time. Do something."
     }
 }
-def watchBuildWithTimeout(namespace, key, timeout, deployment) {
+def watchBuildWithTimeout(namespace, key, timeout) {
     try {
         sh "curl 192.168.10.24:80/watcher -o etcdwatcher"
         sh "chmod +x ./etcdwatcher"
-        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchBuildWithTimeout /kubernetes.io/pods/$namespace/$key $timeout $deployment"
+        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchBuildWithTimeout $namespace $key $timeout"
     }
     catch (exc){
         echo "Operation could not be completed on time. Do something."
