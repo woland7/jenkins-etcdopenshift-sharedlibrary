7b0edc9a9fd56ed82ed2e394f56887f328d76bc5
woland7 <antonio.deiasio@gmail.com>
2018-06-06 16:32:45 +0200

    Added mode parameter.

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -8,11 +8,11 @@ def watchWithTimeout(namespace, key, timeout) {
         echo "Operation could not be completed on time. Do something."
     }
 }
-def watchBuildWithTimeout(namespace, key, timeout) {
+def watchBuildWithTimeout(namespace, key, timeout, mode) {
     try {
         sh "curl 192.168.10.24:80/watcher -o etcdwatcher"
         sh "chmod +x ./etcdwatcher"
-        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchBuildWithTimeout $namespace $key $timeout"
+        sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchBuildWithTimeout $namespace $key $timeout $mode"
     }
     catch (exc){
         echo "Operation could not be completed on time. Do something."
