ee49f1a815a57545ed52d557fde6cd4e1803742a
woland7 <antonio.deiasio@gmail.com>
2018-07-11 11:11:14 +0200

    Changed way of downloading.

--- vars/etcdwatcher.groovy
+++ vars/etcdwatcher.groovy
@@ -1,7 +1,10 @@
+def downloadEtcdWatcher(){
+    sh "curl 192.168.10.36:80/etcdwatcher -o etcdwatcher"
+    sh "chmod +x ./etcdwatcher"
+}
+
 def watchWithTimeout(namespace, key, timeout) {
     try {
-        sh "curl 192.168.10.36:80/etcdwatcher -o etcdwatcher"
-        sh "chmod +x ./etcdwatcher"
         sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchWithTimeout $namespace $key $timeout"
     }
     catch (exc){
@@ -10,11 +13,23 @@ def watchWithTimeout(namespace, key, timeout) {
 }
 def watchBuildWithTimeout(namespace, key, timeout, mode) {
     try {
-        sh "curl 192.168.10.36:80/etcdwatcher -o etcdwatcher"
-        sh "chmod +x ./etcdwatcher"
         sh "./etcdwatcher -cacert ./master.etcd-ca.crt -cert ./master.etcd-client.crt -key ./master.etcd-client.key watchBuildWithTimeout $namespace $key $timeout $mode"
     }
     catch (exc){
         echo "Operation could not be completed on time. Do something."
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
